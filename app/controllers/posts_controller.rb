c5ffa907c227698457bd072d510e7a98db4d308a
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-03-28 18:03:46 +0900

    確認画面作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,10 +11,14 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    if @post.save
-      redirect_to posts_path, notice: "ブログ作成完了"
-    else
+    if params[:back]
       render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
     end
   end
 
@@ -32,11 +36,16 @@ class PostsController < ApplicationController
     end
   end
 
-    def destroy
-      @post.destroy
-      redirect_to posts_path, notice:"ブログを削除しました！"
-    end
-    
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"ブログを削除しました！"
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
   private
 
   def post_params
