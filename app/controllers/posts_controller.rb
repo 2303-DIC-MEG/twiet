d8dd2408705c2735067052f4169e245703ec3c16
yamamuro megumi <norain.norainbow.0223@gmail.com>
2023-03-28 15:33:33 +0900

    ブログ編集画面アップデート

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,8 +21,17 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
-  
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
   private
 
   def post_params
